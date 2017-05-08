6d629e0858a5a96e757dc809a59d6c42339ed3fa
sky <854323752@qq.com>
2017-05-09 01:48:36 +0800

    添加注释

--- joinus/webpack.config.js
+++ joinus/webpack.config.js
@@ -32,10 +32,18 @@ module.exports = {
                 test: /\.(png|jpeg|jpg)$/,
                 use: [
                     {
+                        // url-loader 包含参数
+                        // limit: 表示源文件大于多少时，用`file-loader`处理，否则使用base64编码
+                        // mimetype: 表示base64编码处理的文件类型，如果不设置，将根据文件后缀名进行判断。
                         loader: 'url-loader',
                         options: {
-                            limit: 10000,
-                            name: IMAGE_PATH + '/[name]-[hash:8].[ext]'
+                            limit: 8192, // < 8KB
+                            // file-loader 包含参数
+                            // [path]: 源文件路径（用处不大）
+                            // [name]: 文件名本身
+                            // [hash]: 源文件的hash值
+                            // [ext]: 文件的后缀名
+                            name: IMAGE_PATH + '/[name]-[hash:8].[ext]',
                         }
                     }
                 ]
@@ -43,6 +51,7 @@ module.exports = {
             // sass
             {
                 test: /\.scss$/,
+                // 将文本内容剥离出来，生成独立的文件
                 use: ExtractTextWebpackPlugin.extract({
                     use: [
                         'css-loader',
@@ -54,25 +63,36 @@ module.exports = {
                             }
                         }
                     ],
+                    // 如果无法剥离，使用降级处理，嵌入HTML的<style>标签中
                     fallback: 'style-loader'
                 })
             },
             {
                 test: /\.html$/,
-                use: ['html-loader']
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            attrs: 'img:src img:data-src'
+                        }
+                    }
+                ]
             }
         ]
     },
 
     plugins: [
+        // js压缩工具
         new webpack.optimize.UglifyJsPlugin({
             beautify: true,
             sourceMap: true
         }),
+        // 生成HTML文件
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: 'src/views/index.html'
         }),
+        // 加载器全局配置工具
         new webpack.LoaderOptionsPlugin({
             minimize: true,
             options: {
@@ -84,6 +104,7 @@ module.exports = {
                 ]
             }
         }),
+        // 内容提取工具
         new ExtractTextWebpackPlugin(CSS_PATH + '/[name]-[chunkhash:8].css'),
         // new webpack.HotModuleReplacementPlugin()
     ]
