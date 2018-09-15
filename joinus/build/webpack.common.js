d50b46b00a2ad3efb922b446c42e1cd64812827e
SangBaiPi <sangbaipi@2dfire.com>
2018-09-16 00:36:43 +0800

    优化打包

--- joinus/build/webpack.common.js
+++ joinus/build/webpack.common.js
@@ -8,7 +8,8 @@ module.exports = {
     app: './src/main.js'
   },
   output: {
-    path: path.resolve(__dirname, '../dist')
+    path: path.resolve(__dirname, '../dist'),
+    pathinfo: false
   },
   module: {
     rules: [
