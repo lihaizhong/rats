0e1cc84d63e4427a577dcc7c17aa550c9e1e7e02
SangBaiPi <sangbaipi@2dfire.com>
2018-12-04 17:20:23 +0800

    自定义标签

---
+++ web-components/shadow-doms/index.js
@@ -0,0 +1,28 @@
+function _defineCustomElements (components) {
+  components.forEach(function (vDom) {
+    let name = vDom.name
+    let component = vDom.component
+    let extend = vDom.extends || {}
+
+    window.customElements.define(name, component, extend)
+  })
+}
+
+function _watchCustomElementDefined (components) {
+  let promises = components.map(function (vDom) {
+    return window.customElements.whenDefined(vDom.name)
+  })
+
+  Promise.all(promises)
+    .then(function () {
+      console.log('【CUSTOM ELEMENTS】组件定义完成')
+    })
+    .catch(function (error) {
+      throw error
+    })
+}
+
+function defineElement (components) {
+  _watchCustomElementDefined(components)
+  _defineCustomElements(components)
+}
