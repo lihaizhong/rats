7d75a172f66402a595b44dd5f5e02900572006f1
sky <854323752@qq.com>
2021-09-27 11:55:30 +0800

    feat: 添加serviceworker

--- learn/ServiceWorker/README.md
+++ learn/ServiceWorker/README.md
@@ -5,3 +5,57 @@
 - [使用ServiceWorker](https://developer.mozilla.org/zh-CN/docs/Web/API/Service_Worker_API/Using_Service_Workers)
 - [PWA教程](https://www.bookstack.cn/read/pwa-doc/README.md)
 - [PWA 应用实战](https://lavas-project.github.io/pwa-book/)
+
+## MANIFEST配置
+
+Web应用清单（Web Application Manifest）是一份JSON格式的文件。
+
+网站提供这样一份应用程序清单，并满足一定的生效条件之后，将具有添加到主屏幕的能力。
+
+> 添加到主屏幕的好处有很多，主要在用户黏性和用户体验上。
+
+- 首先是减少了网站入口深度，用户可以直接从主屏幕直达站点。
+- 其次是能够让网站具有更加接近原生体验的特性，具有启动页面，脱离浏览器UI。
+- 最后，添加到主屏幕的网站如同安装原生应用那样，被纳入应用抽屉中，并且可以通过系统设置直接对站点进行设置。
+
+### 引入方式
+
+\<link rel="manifest" href="https://path/to/manifest.json" \/\>
+
+### name或者short_name
+
+必填，用来配置网站应用的名称。
+
+> 其中，name是应用名称，用于应用安装提示、启动页面的显示；
+> short_name是应用名称的简写，用于添加到主屏幕时的应用名展示。
+### start_url
+
+用来定义添加到桌面后的启动URL。可以采用绝对路径和相对路径的方式定义。
+
+> **注意：**目前条件下，浏览器未提供任何值或者环境变量来帮助程序判断页面打开方式。可以通过在`start_url`上添加特殊query来实现。
+### icons
+
+用来定制应用图标。接收一个图标描述对象列表，每个图标的描述对象包含三个属性。
+
+- src: 字符串，图标URL
+- type: 字符串，图标尺寸，格式为`宽x高`，数值单位默认为`px`。对于`.ico`类型的图标支持多种尺寸的情况，可以使用空格作为间隔，如：`48x48 96x96 128x128`。
+- sizes: 字符串，非必填。图标的MIME类型，可以用来让浏览器快速忽略掉不支持的图标类型。
+
+### display
+
+用来定义从桌面打开的网站的显示模式。目前支持以下几种：
+
+| 显示模式 | 描述 | 降级显示类型 |
+- | - | -
+| fullscreen | 全屏模式，页面占满整个屏幕，包括移动设备的状态栏都会被页面覆盖 | standalone |
+| standalone | 独立模式，浏览器相关UI将会被隐藏，移动设备状态栏不会被覆盖 | minimal-ui |
+| minimal-ui | 显示形式与standalone类似，浏览器相关UI会最小化为一个按钮，不同浏览器在实现上略有不同，支持度较低 | browser |
+| browser | 浏览器模式，直接通过浏览器打开网页的普通展示方式，包含地址栏、工具栏等浏览器元素 | None |
+
+### background_color
+
+定义网站背景色，在显示启动页面时生效。
+
+### theme_color
+
+定义网站主题色，会影响到浏览器UI元素的显示。比如启动页面的顶部手机状态栏的颜色。
--- learn/ServiceWorker/service-worker.js
+++ learn/ServiceWorker/service-worker.js
@@ -1,4 +1,4 @@
-const REQUEST_PREFIX = '/';
+const REQUEST_PREFIX = '/'
 const SWUtils = {
   cacheContents: [
     REQUEST_PREFIX,
@@ -10,69 +10,90 @@ const SWUtils = {
     `${REQUEST_PREFIX}static/sw-lifecycle.png`
   ],
   cacheName: 'sw_v3',
-  install(event) {
+  install (event) {
     return caches.open(SWUtils.cacheName)
       .then(cache => cache.addAll(SWUtils.cacheContents))
       .then(() => {
         // 自动跳过等待阶段，一般在开发环境使用
-        self.skipWaiting();
-      });
+        self.skipWaiting()
+      })
   },
-  fetch(event) {
+  fetch (event) {
     return caches.match(event.request).then((cacheResponse) => {
       // 如果有缓存，则直接返回缓存内容
       if (cacheResponse !== null && cacheResponse !== undefined) {
-        return cacheResponse;
+        return cacheResponse
       }
 
       // 如果没有缓存，则通过远程请求获取
       return fetch(event.request).then((response) => {
         // 过滤缓存
         if (event.request.url.indexOf('chrome://')) {
-          return response;
+          return response
         }
 
         return caches.open(SWUtils.cacheName).then((cache) => {
-          cache.put(event.request, response.clone());
+          cache.put(event.request, response.clone())
 
-          return response;
-        });
-      });
+          return response
+        })
+      })
     })
   },
-  activate(event) {
+  activate (event) {
     // 获取所有客户端的控制权
     return self.clients.claim()
       .then(() => caches.keys())
       .then((keyList) => Promise.all(
         keyList.map((key) => {
           if (key !== SWUtils.cacheName) {
-            return caches.delete(key);
+            return caches.delete(key)
           }
+
+          return Promise.resolve()
         })
-      ));
+      ))
   }
-};
+}
 
 self.addEventListener('install', (event) => {
-  console.log('进入install事件');
-  event.waitUntil(SWUtils.install(event));
-});
+  console.log('进入install事件')
+  event.waitUntil(SWUtils.install(event))
+})
 
 self.addEventListener('activate', (event) => {
-  console.log('进入activate事件');
+  console.log('进入activate事件')
   // waitUntil的参数可以是一个Promise对象实例。
-  event.waitUntil(SWUtils.activate(event));
-});
+  event.waitUntil(SWUtils.activate(event))
+})
 
 // 只有在激活之后才能监听到
 self.addEventListener('fetch', (event) => {
-  console.log('进入fetch事件');
+  console.log('进入fetch事件')
   // respondWith的参数可以是Response对象实例，也可以是Promise对象实例。
-  event.respondWith(SWUtils.fetch(event));
-});
+  event.respondWith(SWUtils.fetch(event))
+})
 
 // 只有在激活后才能接收到消息
 self.addEventListener('message', (event) => {
-  console.log('获取到客户端发过来的消息', event);
-});
+  console.log('获取到客户端发过来的消息', event)
+})
+
+/**
+ * 网络推送
+ * 网络推送的建立设计到三端的相互配合。
+ * 1. UA（User Agent），即浏览器。
+ * 2. Push Service，即推送服务器，用于管理推送订阅、消息推送等功能的第三方服务器。（Push Service服务器是浏览器决定的）
+ * 3. Application Server，即网站应用的后端服务。
+ * 从具体推送步骤上进行划分，可以分为订阅（Subscribe）与推送（Push Message）两个部分。
+ * 其中订阅部分包含以下步骤：
+ * 1. Subscribe，浏览器需要向推送服务器发起推送订阅的请求；
+ * 2. Monitor，订阅成功后，浏览器与推送服务器之间会进行通信，同时推送服务器会生成并维护相关订阅信息，在后续的消息推送流程将基于该订阅信息与浏览器保持通信。
+ * 3. Distribute Push Resource，浏览器将推送服务器返回的订阅消息发送给网站后端服务进行保存，服务器将基于该订阅信息向推送服务器发起消息推送。
+ * 而推送部分主要分为两步：
+ * 1. 后端服务通过Web Push向推送服务器发送消息通知，发送时会将前面提到的订阅信息带上，以告知推送服务器这条消息推送的目的地。
+ * 2. 推送服务器接收到消息后，再根据订阅信息将消息推送给对应的浏览器。
+ */
+self.addEventListener('push', (event) => {
+  // node端可以使用`web-push`库来实现push功能。推送消息校验遵循VAPID协议进行身份校验。
+})
