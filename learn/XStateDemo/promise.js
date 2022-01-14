562a56fab82d3b221807d9dd77762b503b563978
sky <854323752@qq.com>
2022-01-14 15:17:51 +0800

    fix: 调整promise

--- learn/XStateDemo/promise.js
+++ learn/XStateDemo/promise.js
@@ -1,27 +1,64 @@
-import { createMachine, interpret } from 'xstate'
+import { createMachine, interpret, assign } from 'xstate'
 
 // Promise实现
 const promiseMachine = createMachine({
   id: 'promise',
   initial: 'pending',
+  context: {
+    test: 'xxx'
+  },
   states: {
     pending: {
       on: {
-        RESOLVE: { target: 'resolved' },
-        REJECT: { target: 'rejected' }
+        RESOLVE: {
+          target: 'resolved',
+          actions: 'resolvingAction'
+        },
+        REJECT: {
+          target: 'rejected',
+          actions: 'rejectingAction'
+        }
       }
     },
     resolved: {
-      type: 'final'
+      type: 'final',
+      invoke: {
+        src: 'resolvedServicer'
+      }
     },
     rejected: {
-      type: 'final'
+      type: 'final',
+      invoke: {
+        src: 'rejectedServicer'
+      }
+    }
+  }
+}, {
+  actions: {
+    resolvingAction: assign((context) => {
+      console.log('【Promise Action】invoke resolving action!')
+    }),
+    rejectingAction: assign((context) => {
+      console.log('【Promise Action】invoke rejecting action!')
+    })
+  },
+  services: {
+    resolvedServicer: () => () => {
+      console.log('【Promise Service】invoke resolved service!')
+    },
+    rejectedServicer: () => (send) => {
+      console.log('【Promise Service】invoke rejected service!')
     }
   }
 })
 
-const promiseService = interpret(promiseMachine).onTransition(state => console.log(state.value))
+const promiseService = interpret(promiseMachine).onTransition(state => console.log(`【Promise Transition】${state.value}`))
 
 promiseService.start()
 
-promiseService.send({ type: 'RESOLVE' })
+promiseService.send({ type: 'RESOLVE' }, 'resolve payload')
+
+// Warning: Event "REJECT" was sent to stopped service "promise". This service has already reached its final state, and will not transition.
+// promiseService.send({ type: 'REJECT' })
+
+promiseService.stop()
