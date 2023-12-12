1f06f16f2863b7706a1c1788054e3f10be48ab2f
sky <854323752@qq.com>
2023-12-13 01:58:24 +0800

    更新ServiceWorker

--- examples/ServiceWorker/index.js
+++ examples/ServiceWorker/index.js
@@ -1,15 +1,15 @@
 const VERSION = "1.0.0";
 const PageUtils = {
   injectVersion(version) {
-    const el = document.querySelector("#version");
+    const elem = document.querySelector("#version");
 
-    el.innerText = `当前版本号：${version}`;
+    elem.textContent = `当前版本号：${version}`;
   },
   injectMessage(message) {
     const el = document.querySelector("#message-container");
     const row = document.createElement("p");
 
-    row.innerText = message;
+    row.textContent = message;
     el.appendChild(row);
   },
 };
@@ -59,7 +59,7 @@ const ServiceWorkerUtils = {
           PageUtils.injectMessage("ServiceWorker正在激活中...");
           break;
         case "activated":
-          PageUtils.injectMessage("ServiceWorker已激活!");
+          PageUtils.injectMessage("ServiceWorker已激活！");
           break;
         case "parsed":
           PageUtils.injectMessage("ServiceWorker已解析！");
@@ -73,6 +73,55 @@ const ServiceWorkerUtils = {
   },
 };
 
+function getBatteryStatus() {
+  if (typeof navigator.getBattery !== 'function') {
+    return null
+  }
+
+  navigator.getBattery()
+    .then((batteryManager) => {
+      // 布尔值，表示设备当前是否正接入电源充电。
+      // 如果设备没有电池，则返回true。
+      document.querySelector('#charging').textContent = batteryManager.charging ? '正在充电' : '使用电池';
+      console.info('设备是否接入电源', batteryManager.charging);
+      // 整数，表示预计离电池充满还有多少秒。
+      // 如果电池已充满或设备没有电池，则返回0。
+      document.querySelector('#charging-time').textContent = batteryManager.chargingTime;
+      console.info('预计充满需要多久（秒）', batteryManager.chargingTime);
+      // 整数，表示预计离电池耗尽还有多少秒。
+      // 如果设备没有电池，则返回Infinity。
+      document.querySelector('#discharging-time').textContent = batteryManager.dischargingTime;
+      console.info('预计电池耗尽时间（秒）', batteryManager.dischargingTime);
+      // 浮点数，表示电量百分比。
+      // 电量完全耗尽返回0.0，电池充满返回1.0。
+      // 如果设备没有电池，则返回1.0。
+      document.querySelector('#level').textContent = (batteryManager.level * 100) + '%';
+      console.info('电量百分比', batteryManager.level);
+
+      // 添加充电状态变化时的处理程序
+      batteryManager.addEventListener('chargingchange', () => {
+        document.querySelector('#charging').textContent = batteryManager.charging ? '正在充电' : '使用电池';
+        console.info('设备是否接入电源', batteryManager.charging);
+      });
+      // 添加充电时间变化时的处理程序
+      batteryManager.addEventListener('chargingtimechange', () => {
+        document.querySelector('#charging-time').textContent = batteryManager.chargingTime;
+        console.info('预计充满需要多久（秒）', batteryManager.chargingTime);
+      });
+      // 添加放电时间变化时的处理程序
+      batteryManager.addEventListener('dischargingtimechange', () => {
+        document.querySelector('#discharging-time').textContent = batteryManager.dischargingTime;
+        console.info('预计电池耗尽时间（秒）', batteryManager.dischargingTime);
+      });
+      // 添加电量百分比变化时的处理程序
+      batteryManager.addEventListener('levelchange', () => {
+        document.querySelector('#level').textContent = (batteryManager.level * 100) + '%';
+        console.info('电量百分比', batteryManager.level);
+      });
+    })
+    .catch((error) => console.error(error));
+}
+
 async function main() {
   PageUtils.injectVersion(VERSION);
 
@@ -83,4 +132,7 @@ async function main() {
   }
 }
 
-main();
+window.onload = () => {
+  main();
+  getBatteryStatus();
+}
--- examples/ServiceWorker/sw.js
+++ examples/ServiceWorker/sw.js
@@ -3,7 +3,7 @@
  * https://developer.chrome.com/docs/workbox/service-worker-overview?hl=zh-cn
  */
 
-const REQUEST_PREFIX = '/'
+const REQUEST_PREFIX = "/";
 const SWUtils = {
   cacheContents: [
     REQUEST_PREFIX,
@@ -12,40 +12,41 @@ const SWUtils = {
     `${REQUEST_PREFIX}static/important-notes.png`,
     `${REQUEST_PREFIX}static/sw-events.png`,
     `${REQUEST_PREFIX}static/sw-fetch.png`,
-    `${REQUEST_PREFIX}static/sw-lifecycle.png`
+    `${REQUEST_PREFIX}static/sw-lifecycle.png`,
   ],
-  cacheName: 'sw_v3',
-  install (event) {
-    return caches.open(SWUtils.cacheName)
-      .then(cache => cache.addAll(SWUtils.cacheContents))
+  cacheName: "sw_v3",
+  install(event) {
+    return caches
+      .open(SWUtils.cacheName)
+      .then((cache) => cache.addAll(SWUtils.cacheContents))
       .then(() => {
         // 自动跳过等待阶段，一般在开发环境使用
-        self.skipWaiting()
-      })
+        self.skipWaiting();
+      });
   },
-  fetch (event) {
+  fetch(event) {
     return caches.match(event.request).then((cacheResponse) => {
       // 如果有缓存，则直接返回缓存内容
       if (cacheResponse !== null && cacheResponse !== undefined) {
-        return cacheResponse
+        return cacheResponse;
       }
 
       // 如果没有缓存，则通过远程请求获取
       return fetch(event.request).then((response) => {
         // 过滤缓存
-        if (event.request.url.indexOf('chrome://')) {
-          return response
+        if (event.request.url.indexOf("chrome://")) {
+          return response;
         }
 
         return caches.open(SWUtils.cacheName).then((cache) => {
-          cache.put(event.request, response.clone())
+          cache.put(event.request, response.clone());
 
-          return response
-        })
-      })
-    })
+          return response;
+        });
+      });
+    });
   },
-  async activate (event) {
+  async activate(event) {
     // https://web.dev/blog/navigation-preload?hl=zh-cn#the-solution
     // 检查是否支持导航预缓存
     if (self.registration.navigationPreload) {
@@ -54,42 +55,38 @@ const SWUtils = {
     }
 
     // 获取所有客户端的控制权
-    await self.clients.claim()
+    await self.clients.claim();
 
-    return Promise.all(
-      caches.keys().map((key) => {
-        if (key !== SWUtils.cacheName) {
-          return caches.delete(key)
-        }
-
-        return Promise.resolve()
+    return caches.open(SWUtils.cacheName).then((cache) =>
+      cache.keys().then((keys) => {
+        keys.forEach((request) => cache.delete(request));
       })
-    )
-  }
-}
+    );
+  },
+};
 
-self.addEventListener('install', (event) => {
-  console.log('进入install事件')
-  event.waitUntil(SWUtils.install(event))
-})
+self.addEventListener("install", (event) => {
+  console.log("进入install事件");
+  event.waitUntil(SWUtils.install(event));
+});
 
-self.addEventListener('activate', (event) => {
-  console.log('进入activate事件')
+self.addEventListener("activate", (event) => {
+  console.log("进入activate事件");
   // waitUntil的参数可以是一个Promise对象实例。
-  event.waitUntil(SWUtils.activate(event))
-})
+  event.waitUntil(SWUtils.activate(event));
+});
 
 // 只有在激活之后才能监听到
-self.addEventListener('fetch', (event) => {
-  console.log('进入fetch事件')
+self.addEventListener("fetch", (event) => {
+  console.log("进入fetch事件");
   // respondWith的参数可以是Response对象实例，也可以是Promise对象实例。
-  event.respondWith(SWUtils.fetch(event))
-})
+  event.respondWith(SWUtils.fetch(event));
+});
 
 // 只有在激活后才能接收到消息
-self.addEventListener('message', (event) => {
-  console.log('获取到客户端发过来的消息', event)
-})
+self.addEventListener("message", (event) => {
+  console.log("获取到客户端发过来的消息", event);
+});
 
 /**
  * 网络推送
@@ -106,6 +103,6 @@ self.addEventListener('message', (event) => {
  * 1. 后端服务通过Web Push向推送服务器发送消息通知，发送时会将前面提到的订阅信息带上，以告知推送服务器这条消息推送的目的地。
  * 2. 推送服务器接收到消息后，再根据订阅信息将消息推送给对应的浏览器。
  */
-self.addEventListener('push', (event) => {
+self.addEventListener("push", (event) => {
   // node端可以使用`web-push`库来实现push功能。推送消息校验遵循VAPID协议进行身份校验。
-})
+});
