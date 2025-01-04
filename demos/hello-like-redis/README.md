7a8e1b3adefaf85dbcec99dacc08bebed7f34ce7
lihzsky <lihaizh_cn@foxmail.com>
2025-01-05 01:27:30 +0800

    feat: 修改demos

---
+++ demos/hello-like-redis/README.md
@@ -0,0 +1,7 @@
+# 实现一个redis的缓存服务器
+
+## 参考资料
+
+- [Node.js结合ProtoBuffer,从零实现一个redis! ](https://segmentfault.com/a/1190000023009675)
+- [图解protobuf的编码varints,zigzag与数据组织形式](https://juejin.cn/post/7267780768982908986)
+- [ProtoBuf 入门教程](https://www.tizi365.com/archives/367.html)
--- demos/hello-sse/src/main.mjs
+++ demos/hello-sse/src/main.mjs
@@ -1,20 +1,44 @@
 import http from "node:http";
-import config from "./config.mjs";
-import routes, { assetsCallback } from "./router.mjs";
+import path from "node:path";
+import fs from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import Router from "find-my-way";
+import { ServerSentEvents } from "./sse.mjs";
 
-const server = http.createServer(async (request, response) => {
-  // 查询路由
-  const requestCallback = routes[request.url];
+const router = new Router();
+const port = 10086
 
-  if (typeof requestCallback === "function") {
-    // 执行预设方案
-    requestCallback(request, response);
-  } else {
-    // 执行降级方案
-    assetsCallback(request, response);
+router.on("GET", "/subscribe", (req, res) => {
+  const sse = new ServerSentEvents(req, res);
+  // Send a message on connection
+  sse.connect();
+  // Send a subsequent message every five seconds
+  setInterval(() => {
+    sse.send(new Date().toLocaleString());
+  }, 5000);
+  // Close the connection when the client disconnects
+  req.on("close", () => sse.close());
+});
+
+router.on("GET", "/pages/*", async (req, res) => {
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const assetsPath = path.join(__dirname, "..", req.url);
+    const data = await fs.readFile(assetsPath, "binary");
+
+    res.end(data, "binary");
+  } catch (ex) {
+    console.error("Failed to obtain static resources.", ex);
+    res.setHeader("Content-Type", "text/html;charset=utf-8");
+    res.end("静态资源读取失败！");
   }
 });
 
-server.listen(config.PORT, () => {
-  console.log(`Server started at ${config.PORT}`);
+const server = http.createServer(async (req, res) => {
+  router.lookup(req, res);
+});
+
+server.listen(port, () => {
+  console.log(`Server started at ${port}`);
 });
